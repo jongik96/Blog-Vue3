4e619a952999c2479036880053ccef8a4144a6af
jongik96 <pji3504@naver.com>
2022-01-12 16:24:46 +0900

    초기환경설정

---
+++ vue3-blog/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import navBar from './components/navBar.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      navBar,
+    }
+  }
+</script>
+
+<template>
+  <navBar></navBar>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-blog/src/components/navBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="grid grid-cols-6">
+      <div class="grid col-start-2 col-span-4 bg-green-200 rounded-md">
+          <a href="/" class="col-start-1 col-span-1 text-xl font-bold">JongIk's Blog</a>
+          <ul class="flex col-start-2 col-span-3 mt-2">
+                <li class="" v-for="(menuItem, id) in menus" :key="id">
+                    <button class="text-gray-500 hover:bg-green-500 hover:font-bold rounded-md mr-3 text-gray-500" @click="onMovePage($event, menuItem)">
+                        {{ menuItem.value }}
+                    </button>
+                </li>
+          </ul>
+      </div>
+  </nav>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+export default {
+    name: 'navBar',
+    data(){
+        return{
+            menus: [
+                        {
+                            key: 'Home', value: 'Home', URL: '#'
+                        },
+                        {
+                            key: 'App', value: 'App', URL: '#'
+                        },
+                        {
+                            key: 'Profile', value: 'Profile', URL: '#'
+                        },
+                    ]
+        }
+    }
+    ,
+    setup(){
+        const menu = ref('home');
+        const onMovePage = (evt, menuItem) =>{
+            if(evt){
+                evt.preventDefault();
+            }
+            menuItem.value = menuItem.key
+        }
+
+        return {
+            menu,
+            onMovePage
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
