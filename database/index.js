4690e3f02a90a7e87b36890e6e8314c1646c3cfb
jongik96 <pji3504@naver.com>
2022-01-12 16:31:25 +0900

    Express sqlite3 서버 임시생성

--- database/index.js
+++ database/index.js
@@ -1,1 +1,39 @@
-let sq;
+let sqlite3 = require("sqlite3");
+const sqlite3 = require("sqlite3");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.disable("x-powered-by");
+app.use(cors());
+
+const PORT = 8000;
+let db = new sqlite3.Database("database.db", (err) => {
+  if (!err) {
+    db.run("CREATE TABLE tbl_about_myself (name text, email text)", (err2) => {
+      if (!err2) {
+        db.run("INSERT INTO tbl_about_myself (name, email) VALUES ('JONGIK', 'pji3504@naver.com')");
+      }
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening... ${PORT}`);
+});
+
+app.get("/", (req, res, next) => {
+  res.json({ rsp: "ok" });
+});
+
+app.get("/db/about-me", (req, res, next) => {
+  db.all("SELECT * FROM tbl_about_myself", (err, rows) => {
+    if (!err) {
+      res.json({
+        rsp: "ok",
+        data: rows[0],
+      });
+    } else {
+      console.log("Error Query");
+    }
+  });
+});
