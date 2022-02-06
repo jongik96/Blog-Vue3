a97292b67be77dffbf890686f2d27e4cf66cea08
jongik96 <pji3504@naver.com>
2022-02-06 21:35:17 +0900

    vuex 수정

--- database/index.js
+++ database/index.js
@@ -9,11 +9,28 @@ app.use(cors());
 const PORT = 8000;
 let db = new sqlite3.Database("database.db", (err) => {
   if (!err) {
-    db.run("CREATE TABLE tbl_about_myself (name text, email text)", (err2) => {
+    db.run("CREATE TABLE tbl_about_myself (name TEXT, email TEXT, UNIQUE(name,email))", (err2) => {
       if (!err2) {
-        db.run("INSERT INTO tbl_about_myself (name, email) VALUES ('JONGIK', 'pji3504@naver.com')");
+        db.run("INSERT OR IGNORE INTO tbl_about_myself (name, email) VALUES ('JONGIK', 'pji3504@naver.com')");
       }
     });
+    db.run('CREATE TABLE IF NOT EXISTS tbl_my_resume (data DATE, title TEXT, content TEXT, URL TEXT, UNIQUE(date, title))',
+      (err2) => {
+        if (!err2) {
+          const resume = [
+            {
+              date: '1996-09-10',
+              title: '출생',
+              content: '응애',
+              URL: 'jongik.tistory.com'
+            },
+          ]
+          resume.forEach((item) => {
+            const query = `INSERT OR IGNORE INTO tbl_my_resume (date,title,content,URL) VALUES ('${item.date}', '${item.title}', '${item.content}', '${item.URL}')`
+            db.run(query)
+          })
+        }
+      })
   }
 });
 
@@ -26,14 +43,41 @@ app.get("/", (req, res, next) => {
 });
 
 app.get("/db/about-me", (req, res, next) => {
-  db.all("SELECT * FROM tbl_about_myself", (err, rows) => {
+  let result = {
+    rsp: 'fail',
+  }
+  db.get('SELECT * FROM tbl_about_myself', (err, row) => {
     if (!err) {
-      res.json({
-        rsp: "ok",
-        data: rows[0],
+      result.data = row
+      db.all("SELECT * FROM tbl_about_resume ORDER BY date desc", (err2, rows) => {
+        if (!err2) {
+          result.rsp = 'ok'
+          result.data.resume = rows
+          res.json(result)
+        } else {
+          res.json(result)
+        }
       });
     } else {
-      console.log("Error Query");
+      res.json(result);
     }
-  });
+  })
+
 });
+
+db.get('SELECT * FROM tbl_about_myself', (err, row) => {
+  if (!err) {
+    result.data = row;
+    db.all('SELECT * FROM tbl_my_resume ORDER BY date desc', (err2, rows) => {
+      if (!err2) {
+        result.rsp = 'ok'
+        result.data.resume = rows
+        res.json(result);
+      } else {
+        res.json(result)
+      }
+    })
+  } else {
+    res.json(result)
+  }
+})
